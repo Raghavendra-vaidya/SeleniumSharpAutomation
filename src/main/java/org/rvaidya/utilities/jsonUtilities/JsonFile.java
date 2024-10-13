25b92dd20188214ec7486fe267a83fdcad31a6b5
Raghavendra Vaidya <rkvaidya58@gmail.com>
2024-10-13 19:14:09 +0530

    update utilities :zap:

--- src/main/java/org/rvaidya/utilities/jsonUtilities/JsonFile.java
+++ src/main/java/org/rvaidya/utilities/jsonUtilities/JsonFile.java
@@ -1,4 +1,42 @@
 package org.rvaidya.utilities.jsonUtilities;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class JsonFile {
+
+    public synchronized JsonObject getJsonFileObject(String filePath) {
+        JsonObject data = null;
+        Path path = Paths.get(filePath);
+        try {
+            Reader reader = Files.newBufferedReader(path);
+            JsonElement jsonElement = JsonParser.parseReader(reader);
+            data = jsonElement.getAsJsonObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public synchronized JsonArray getJsonFileArray(String filePath) {
+        JsonArray data = null;
+        Path path = Paths.get(filePath);
+        try {
+            Reader reader = Files.newBufferedReader(path);
+            JsonElement jsonElement = JsonParser.parseReader(reader);
+            data = jsonElement.getAsJsonArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+    
+
 }
