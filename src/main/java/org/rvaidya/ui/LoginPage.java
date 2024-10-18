66623c06059badbcd3a4a2a0879a6b84160cbd39
Raghavendra vaidya <rkvaidya58@gmail.com>
2024-10-18 22:07:29 +0530

    add login page objects (#7)

--- src/main/java/org/rvaidya/ui/LoginPage.java
+++ src/main/java/org/rvaidya/ui/LoginPage.java
@@ -8,14 +8,58 @@ import org.rvaidya.utilities.jsonUtilities.JsonFile;
 
 public class LoginPage {
     WebDriver driver;
-    JsonObject loginIdentifiers = JsonFile.getJsonFileObject("src/main/resources/loginIdentifiers.json");
+    static JsonObject loginIdentifiers = JsonFile.getJsonFileObject("src/main/resources/loginIdentifiers.json");
 
+    /* Constructor */
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public WebElement getUsernameField() {
-        return driver.findElement(By.xpath(loginIdentifiers.get("usernameFieldXpath").toString()));
+    /* Elements */
+    public synchronized WebElement getHeaderTextElement() {
+        return driver.findElement(By.cssSelector(loginIdentifiers.get("headerCss").getAsString()));
+    }
+
+    public synchronized WebElement getUsernameFieldLabel() {
+        return driver.findElement(By.xpath(loginIdentifiers.get("usernameLabelXpath").getAsString()));
+    }
+
+    public synchronized WebElement getPasswordFieldLabel() {
+        return driver.findElement(By.xpath(loginIdentifiers.get("passwordFieldXpath").getAsString()));
+    }
+
+    public synchronized WebElement getUsernameField() {
+        return driver.findElement(By.xpath(loginIdentifiers.get("usernameFieldXpath").getAsString()));
+    }
+
+    public synchronized WebElement getPassworField() {
+        return driver.findElement(By.xpath(loginIdentifiers.get("passwordFieldXpath").getAsString()));
+    }
+
+    public synchronized WebElement getLoginButton() {
+        return driver.findElement(By.xpath(loginIdentifiers.get("loginButtonXpath").getAsString()));
+    }
+
+    public synchronized WebElement getForgotPasswordLink() {
+        return driver.findElement(By.xpath(loginIdentifiers.get("forgotPasswordLinkXpath").getAsString()));
+    }
+
+    /* Action */
+
+    public synchronized void setUsername(String username) {
+        getUsernameField().sendKeys(username);
+    }
+
+    public synchronized void setPassword(String password) {
+        getPassworField().sendKeys(password);
+    }
+
+    public synchronized void clickOnLogin() {
+        getLoginButton().click();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(loginIdentifiers.get("forgotPasswordLinkXpath").getAsString());
     }
 
 
