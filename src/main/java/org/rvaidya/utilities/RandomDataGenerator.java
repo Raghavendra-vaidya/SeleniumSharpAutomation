3c104c8d566a9553e5ba4810d10fa4c5860a6bb0
Raghavendra Vaidya <rkvaidya58@gmail.com>
2024-10-19 07:58:18 +0530

    add utils to generate unique person and animal names

--- src/main/java/org/rvaidya/utilities/RandomDataGenerator.java
+++ src/main/java/org/rvaidya/utilities/RandomDataGenerator.java
@@ -1,4 +1,40 @@
 package org.rvaidya.utilities;
 
+import net.datafaker.Faker;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.function.Supplier;
+
 public class RandomDataGenerator {
+
+    public synchronized static String getMaleName() {
+        Supplier<String> supplier = () -> {
+
+            String randomString = RandomStringUtils.random(6, true, true);
+            Faker faker = new Faker();
+            return faker.name().malefirstName() + "_" + randomString;
+        };
+        return supplier.get();
+    }
+
+    public synchronized static String getFemaleName() {
+        Supplier<String> supplier = () -> {
+
+            String randomString = RandomStringUtils.random(6, true, true);
+            Faker faker = new Faker();
+            return faker.name().femaleFirstName() + "_" + randomString;
+        };
+        return supplier.get();
+    }
+
+    public synchronized static String getAnimalName() {
+        Supplier<String> supplier = () -> {
+
+            String randomString = RandomStringUtils.random(6, true, true);
+            Faker faker = new Faker();
+            return faker.animal().name() + "_" + randomString;
+        };
+        return supplier.get();
+    }
+
 }
