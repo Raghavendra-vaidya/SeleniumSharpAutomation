ddf89aa49b8f659b5bd8dac394e7d36ad1ae4e04
Raghavendra Vaidya <rkvaidya58@gmail.com>
2024-10-24 21:20:29 +0530

    add simple test and add testNg.xml file :zap:

--- src/test/java/org/rvaidya/LoginTest/LoginTest.java
+++ src/test/java/org/rvaidya/LoginTest/LoginTest.java
@@ -1,4 +1,36 @@
 package org.rvaidya.LoginTest;
 
-public class LoginTest {
+import org.openqa.selenium.WebDriver;
+import org.rvaidya.ui.LoginPage;
+import org.rvaidya.utilities.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = setUpDriver("Chrome");
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test(description = "Verify page title")
+    public void testPageTitle() {
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+    }
+
+    @Test(description = "Verify username field is displayed")
+    public void testUsernameFieldIsDisplayed() {
+        Assert.assertTrue(loginPage.getUsernameField().isDisplayed());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+        clearDriver();
+    }
 }
