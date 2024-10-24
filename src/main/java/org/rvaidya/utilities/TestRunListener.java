47edbb7c04a7ae10595800a97f175df4168339c2
Raghavendra vaidya <rkvaidya58@gmail.com>
2024-10-24 20:35:21 +0530

    implement logs :spark: (#10)

--- src/main/java/org/rvaidya/utilities/TestRunListener.java
+++ src/main/java/org/rvaidya/utilities/TestRunListener.java
@@ -6,6 +6,8 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.rvaidya.utilities.reportUtils.ExtentReportManager;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -16,9 +18,11 @@ import java.util.regex.Pattern;
 public class TestRunListener implements ITestListener {
     private static ExtentReports extent = ExtentReportManager.createInstance();
     private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
+    private static final Logger log = LogManager.getLogger(TestRunListener.class);
 
     @Override
     public void onTestStart(ITestResult result) {
+        log.info("Test Started : " + result.getName());
         ExtentTest test = extent.createTest(result.getMethod().getMethodName());
         extentTest.set(test);
         String testClassFull[] = result.getInstanceName().split(Pattern.quote("."));
@@ -28,13 +32,17 @@ public class TestRunListener implements ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult result) {
+        log.info("Test Passed : " + result.getName());
         String logText = "<b>Test method " + result.getMethod().getMethodName() + " Successful</b>";
         Markup mark = MarkupHelper.createLabel(logText, ExtentColor.GREEN);
         extentTest.get().log(Status.PASS, mark);
+
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        log.error("Test Failed : " + result.getName());
+        log.error("Error : " + result.getThrowable());
         String exceptionMsg = result.getThrowable().toString();
         extentTest.get().fail("<details><summary><b><font color=red>" + " Exception occurred click to see the details:"
                 + "</font></b></summary>" + exceptionMsg.replaceAll(",", "<br>") + "</details> \n");
@@ -46,6 +54,7 @@ public class TestRunListener implements ITestListener {
 
     @Override
     public void onTestSkipped(ITestResult result) {
+        log.warn("Test Skipped: " + result.getName());
         String logText = "<b>Test method " + result.getMethod().getMethodName() + " Skipped</b>";
         Markup mark = MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
         extentTest.get().log(Status.SKIP, mark);
@@ -63,11 +72,13 @@ public class TestRunListener implements ITestListener {
 
     @Override
     public void onStart(ITestContext context) {
+        log.info("Starting Test Execution: " + context.getName());
         ITestListener.super.onStart(context);
     }
 
     @Override
     public void onFinish(ITestContext context) {
+        log.info("Finished Test Execution: " + context.getName());
         if (extent != null) extent.flush();
     }
 }
