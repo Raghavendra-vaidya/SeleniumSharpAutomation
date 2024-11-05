912f6fccdedfdfaca45f2131b9b6731c8421a6f0
Raghavendra Vaidya <rkvaidya58@gmail.com>
2024-11-05 23:34:42 +0530

    update utils

--- src/main/java/org/rvaidya/utilities/PropertiesUtils.java
+++ src/main/java/org/rvaidya/utilities/PropertiesUtils.java
@@ -1,10 +1,21 @@
 package org.rvaidya.utilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Properties;
 
 public class PropertiesUtils {
+    private static final Logger log = LogManager.getLogger(TestRunListener.class);
 
     public synchronized static Properties getPropertiesFileInstance(String fileLocation) {
         Properties properties = null;
@@ -32,6 +43,29 @@ public class PropertiesUtils {
         }
         return data;
 
+    }
+
+    public static synchronized void takeScreenShotOnFailure(WebDriver driver, ITestResult result) {
+        int status = result.getStatus();
+
+        if (status == ITestResult.SUCCESS || status == ITestResult.SKIP) {
+            return;
+        }
+
+        File destnFolder = new File("./Screenshots/");
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File src = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        File destn = new File("./Screenshots/" + result.getName() + "jpg");
+
+        try {
+            if (!destnFolder.exists()) destnFolder.mkdir();
+            Files.copy(src.toPath(), destn.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(e.toString());
+        }
     }
 
 
